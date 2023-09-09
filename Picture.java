878cc3956cedaf23a7280954fea297ffdc67ab76
omaremalik <128064925+omaremalik@users.noreply.github.com>
2023-09-08 20:52:36 -0400

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -1,84 +1,124 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ * Two people throwing a triangle. 
  *
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author: Omar Malik
+ *
+ * @version 2023.09.08
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
     private Circle sun;
+    private Square grass;
+    private Square sky;
+    private Person person1;
+    private Person person2;
+    private Triangle triangle; // Add a Triangle object
+    private Circle cloud1; // First circle for the cloud
+    private Circle cloud2; // Second circle for the cloud
+    private Circle cloud3; // Third circle for the cloud
     private boolean drawn;
 
-    /**
-     * Constructor for objects of class Picture
-     */
     public Picture()
-    {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
+    { 
         sun = new Circle();
+        grass = new Square();
+        sky = new Square();
+        person1 = new Person();
+        person2 = new Person();
+        triangle = new Triangle(); // Initialize the triangle
+        cloud1 = new Circle(); // Initialize the first circle for the cloud
+        cloud2 = new Circle(); // Initialize the second circle for the cloud
+        cloud3 = new Circle(); // Initialize the third circle for the cloud
         drawn = false;
     }
 
-    /**
-     * Draw this picture.
-     */
     public void draw()
     {
-        if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
-            
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
-    
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
-    
+        if (!drawn) {
+            // Draw the sky 
+            sky.changeColor("blue");
+            sky.moveHorizontal(-320);
+            sky.moveVertical(-140);
+            sky.changeSize(1000);
+            sky.makeVisible();
+
+            // Draw the grass 
+            grass.changeColor("green");
+            grass.moveHorizontal(-320);
+            grass.moveVertical(140);
+            grass.changeSize(1000);
+            grass.makeVisible();
+
+            // Move the sun 
             sun.changeColor("yellow");
-            sun.moveHorizontal(100);
+            sun.moveHorizontal(200);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            // Draw the first person 
+            person1.moveHorizontal(-120);
+            person1.moveVertical(30);
+            person1.makeVisible();
+
+            // Draw the triangle 
+            triangle.changeColor("red"); // Set the color of the triangle
+            triangle.changeSize(20, 20); // Set the size of the triangle
+            triangle.moveHorizontal(10); // Adjust horizontal position between persons
+            triangle.moveVertical(25); // Adjust position between persons
+            triangle.makeVisible();
+
+            // Draw the second person 
+            person2.moveHorizontal(50);
+            person2.moveVertical(30);
+            person2.makeVisible();
+
+            // Draws the clouds
+            cloud1.changeColor("white"); // Set the color of the cloud
+            cloud1.changeSize(40); // Set the size of the first circle
+            cloud1.moveHorizontal(-100); // Adjust horizontal position for the first circle
+            cloud1.moveVertical(-50); // Adjust vertical position for the first circle
+            cloud1.makeVisible();
+
+            cloud2.changeColor("white");
+            cloud2.changeSize(40);
+            cloud2.moveHorizontal(-80); // Adjust horizontal position for the second circle
+            cloud2.moveVertical(-50); // Adjust vertical position for the second circle
+            cloud2.makeVisible();
+
+            cloud3.changeColor("white");
+            cloud3.changeSize(40);
+            cloud3.moveHorizontal(-60); // Adjust horizontal position for the third circle
+            cloud3.moveVertical(-50); // Adjust vertical position for the third circle
+            cloud3.makeVisible();
+
             drawn = true;
         }
     }
 
-    /**
-     * Change this picture to black/white display
-     */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
         sun.changeColor("black");
+        grass.changeColor("black");
+        sky.changeColor("black");
+        person1.changeColor("black");
+        person2.changeColor("black");
+        triangle.changeColor("black"); // Set the triangle color to black
+        cloud1.changeColor("black"); // Set the cloud color to black
+        cloud2.changeColor("black");
+        cloud3.changeColor("black");
     }
 
-    /**
-     * Change this picture to use color display
-     */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
         sun.changeColor("yellow");
+        grass.changeColor("green");
+        sky.changeColor("blue");
+        person1.changeColor("black");
+        person2.changeColor("black");
+        triangle.changeColor("red");
+        cloud1.changeColor("white"); // Set the cloud color to white
+        cloud2.changeColor("white");
+        cloud3.changeColor("white");
     }
 }
